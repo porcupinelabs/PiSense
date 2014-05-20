7390d1aae9da48bb9a7d8e6877b3aa3e8c515726
root <root@raspberrypi.(none)>
2014-05-20 01:48:35 +0000

    corrected 2 errors - /boot path, and missing quote after escape sequence

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 apt-get install python-serial 
 sed -i '/ttyAMA0/s/^/#/' /etc/inittab
-sed -i 's/ .*=ttyAMA0.[0-9]* / /' cmdline.txt
+sed -i 's/ .*=ttyAMA0.[0-9]* / /' /boot/cmdline.txt
 
 echo "# Start PiSense" >> /etc/rc.local
 echo "cd ~pi/PiSense/pslog"
 echo "printf \"starting PiSense datalogger...\\n\"" >> /etc/rc.local
 echo "python pslog.py > pslog.log 2>&1 &" >> /etc/rc.local
-echo "printf \"starting PiSense web server...\\n\" >> /etc/rc.local
+echo "printf \"starting PiSense web server...\\n\"" >> /etc/rc.local
 echo "cd ~pi/PiSense/psweb" >> /etc/rc.local
 echo "python psweb.py > psweb.log 2>&1 &" >> /etc/rc.local
 echo "exit 0" >> /etc/rc.local
