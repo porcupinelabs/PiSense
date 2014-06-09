d6fddac23d0931850d1c19b998b024cb70e841f4
porcupinelab1 <info@porcupineelectronics.com>
2014-06-09 22:24:40 +0000

    updating since algorithm is in the firmware

--- pslog/Heading.py
+++ pslog/Heading.py
@@ -1,21 +1,10 @@
-import math
 
 def postprocess(params, readings):
     try:
-        scale=params[0];
-        accx=float(readings[0])/scale *-1 #axis opposite on the devices
-        accy=float(readings[1])/scale *-1
-        accz=float(readings[2])/scale
-        x=float(readings[3])
-        y=float(readings[4])
-        z=float(readings[5])
-        roll=math.asin(accy)#in radians
-        pitch=math.asin(accx)
-        xc= x * math.cos(pitch) - z * math.sin(pitch)
-        yc = x * math.sin(roll) * math.sin(pitch) + y * math.cos(roll) - z* math.sin(roll) * math.cos(pitch)
-        tan = yc/xc
-        heading = format( ( math.atan(tan) * 180 / 3.1415927) + 180, '.1f')
-    except:
-        heading=-1
-        print scale,accx,accy,accz,x,y,z,roll,pitch,xc,yc
+        declination = int(params[0])
+        heading=int(readings[0])
+        heading += declination
+        heading %= 360
+    except :
+        heading = (-1)
     return str(heading)
